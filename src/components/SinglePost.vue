5b8f0d3f25c4e95ecd05f4249f8f5559b017dc59
Syed Faraz Khalid <36688688+Farazzaidi22@users.noreply.github.com>
2021-07-12 04:33:22 +0500

    created a single post composable

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="post">
-     <h3>{{ post.title }}</h3>
-     <p>{{ snippet}}</p>
-     <span v-for="tag in post.tags" :key="tag">
-         #{{tag}}
-     </span>
+    <router-link :to="{ name: 'Details', params: {id: post.id}}">
+        <h3>{{ post.title }}</h3>
+    </router-link>
+
+    <p>{{ snippet}}</p>
+        <span v-for="tag in post.tags" :key="tag">
+            #{{tag}}
+        </span>
   </div>
 </template>
 
